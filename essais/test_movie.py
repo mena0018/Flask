8956c38900ad37247648d1b7161d6b6e1bc27e14
Menad <rabie.menad@etudiant.univ-reims.fr>
2022-01-11 21:48:59 +0100

    Ajout des tests pour ratingToStars + optimisation de l'affichage dans la console

--- essais/test_movie.py
+++ essais/test_movie.py
@@ -1,6 +1,16 @@
 # essais/test_movie.py
-from movie import durationToString
+from movie import durationToString, ratingToStars
+
+print ("\nDurationToString")
+print ("----------------")
 
 print(durationToString(512))
 print(durationToString(4096))
 print(durationToString(32768))
+
+print ("\nratingToStars")
+print ("--------------")
+print(ratingToStars(0))
+print(ratingToStars(2))
+print(ratingToStars(4))
+print(ratingToStars(8))
