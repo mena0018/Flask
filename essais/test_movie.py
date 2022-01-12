e909cd14e2fc57c1e38bb1de19935392cd282bd5
Rabie Menad <rabie.menad@etudiant.univ-reims.fr>
2022-01-12 14:27:58 +0100

    Test valide de la classe movie

--- essais/test_movie.py
+++ essais/test_movie.py
@@ -1,17 +1,23 @@
 # essais/test_movie.py
 from movie import durationToString, ratingToStars
+from movie import Movie
 
-print ("\nDurationToString")
-print ("----------------")
+print("\nDurationToString")
+print("----------------")
 
 print(durationToString(512))
 print(durationToString(4096))
 print(durationToString(32768))
 
-print ("\nratingToStars")
-print ("--------------")
+print("\nratingToStars")
+print("--------------")
 print(ratingToStars(0))
 print(ratingToStars(2))
 print(ratingToStars(4))
 print(ratingToStars(8))
-print(ratingToStars(100))
+print(ratingToStars(100), "\n")
+
+# ...
+m1 = Movie('Les évadés')
+print(vars(m1))
+print(dir(m1), "\n")
