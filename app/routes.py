aadf8ff2eee91dcc1553c80df08f4a8fd3a8dd5d
Rabie Menad <rabie.menad@etudiant.univ-reims.fr>
2022-01-19 15:21:28 +0100

    Ajout des messages postés

--- app/routes.py
+++ app/routes.py
@@ -6,4 +6,15 @@ from app import app
 @app.route('/index')
 def index() -> str:
     user = {'username': 'Philippe'}
-    return render_template('index.html', title='Page principale', user=user)
+    # return render_template('index.html', title='Page principale', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': "Flask, c'est super !"
+        },
+        {
+            'author': {'username': 'Suzan'},
+            'body': "C'est encore mieux que Symfony ! Oups !"
+        }
+    ]
+    return render_template('index.html', title='Accueil', user=user, posts=posts)
