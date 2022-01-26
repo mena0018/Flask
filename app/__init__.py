8dcdfd51dff4f8c8f5eceba2b6f9088d1e9d8c89
Rabie Menad <rabie.menad@etudiant.univ-reims.fr>
2022-01-26 14:33:28 +0100

    Migration de la base de données

--- app/__init__.py
+++ app/__init__.py
@@ -7,12 +7,11 @@ from flask_migrate import Migrate
 app = Flask(__name__)
 app.config.from_object(Config)
 
-# On importe le fichier contenant la définition des fonctions de vue
-# ainsi que celui des models
-from app import routes, models
-
 # Démarrage du moteur de la base de données
 db = SQLAlchemy(app)
 # Démarrage de l'outil de migration associé à la base de données
 migrate = Migrate(app, db)
 
+# On importe le fichier contenant la définition des fonctions de vue
+# ainsi que celui des models
+from app import routes, models
