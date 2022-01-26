085dd158476a306238f6766b61d83153202ad38a
Rabie Menad <rabie.menad@etudiant.univ-reims.fr>
2022-01-26 14:00:16 +0100

    Configuration de la base de données

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,18 @@
 from flask import Flask
 from app.config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 # Création de l’application
 app = Flask(__name__)
-
 app.config.from_object(Config)
 
 # On importe le fichier contenant la définition des fonctions de vue
-from app import routes
+# ainsi que celui des models
+from app import routes, models
+
+# Démarrage du moteur de la base de données
+db = SQLAlchemy(app)
+# Démarrage de l'outil de migration associé à la base de données
+migrate = Migrate(app, db)
+
--- app/config.py
+++ app/config.py
@@ -1,9 +1,17 @@
 # Classe de configuration
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 # Dans le code ci-dessus, on recherche une variable d’environnement portant le nom SECRET_KEY qu’on
 # affecte à l’attribut de classe SECRET_KEY. Si la variable d’environnement n’existe pas, c’est la chaîne de
 # caractères 'un mot de passe à garder secret' qui sera utilisé.
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'un mot de passe à garder secret'
+
+    # Configuration de la base de données
+    SQLALCHEMY_DATABASE_URI = \
+        os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
